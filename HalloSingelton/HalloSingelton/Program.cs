8c6dd6ce5bcca6c7f92934533491cb6bae7f029d
André <andrerrr@outlook.de>
2018-11-28 12:00:46 +0100

    Default Singelton ist nicht ThreadSafe

---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() => Logger.Instance.Log($"Was wollen wir trinken? {i} Tage lang..."));
+            }
+
+            Logger.Instance.Log("Hello World!");
+            Logger.Instance.Log("Was geht ab?");
+            Logger.Instance.Log("Heute trinken wir Kölsch!");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Logger
+    {
+        static int instCounter = 0;
+        int instId = 0;
+        private static Logger instance = null;
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Logger();
+
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+
+            instCounter++;
+            instId = instCounter;
+        }
+
+
+        public void Log(string txt)
+        {
+            Console.WriteLine($"({instId}/{instCounter}) [{DateTime.Now}] {txt}");
+        }
+    }
+}
