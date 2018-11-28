89a98e275f4316e7f4ec576f2d9d25e0df28a4e1
André <andrerrr@outlook.de>
2018-11-28 15:49:02 +0100

    mmmmh, lecker Decorator

---
+++ HalloDeco/HalloDeco/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace HalloDeco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var frühstück = new Salami(new Käse(new Brot()));
+            Console.WriteLine($"{frühstück.Name} {frühstück.Preis:c}");
+
+            var mittag = new Salami(new Käse(new Käse(new Käse(new Pizza()))));
+            Console.WriteLine($"{mittag.Name} {mittag.Preis:c}");
+
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ICompo
+    {
+        string Name { get; }
+        decimal Preis { get; }
+    }
+
+    abstract class Deco : ICompo
+    {
+        protected ICompo parent;
+        public Deco(ICompo parent) => this.parent = parent;
+
+        public abstract string Name { get; }
+
+        public abstract decimal Preis { get; }
+    }
+
+    class Pizza : ICompo
+    {
+        public string Name => "Pizza";
+
+        public decimal Preis => 5m;
+    }
+
+    class Brot : ICompo
+    {
+        public string Name => "Brot";
+
+        public decimal Preis => 2m;
+    }
+
+    class Käse : Deco
+    {
+        public Käse(ICompo parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Käse";
+
+        public override decimal Preis => parent.Preis + 1;
+    }
+
+    class Salami : Deco
+    {
+        public Salami(ICompo parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Salami";
+
+        public override decimal Preis => parent.Preis + 1.7m;
+    }
+}
