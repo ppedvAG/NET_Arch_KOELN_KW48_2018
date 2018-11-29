c9df69f266be81d6109d4beade394d7fb890934c
André <andrerrr@outlook.de>
2018-11-29 15:32:11 +0100

    Fertig

---
+++ ppedv.TastyMoon/ppedv.TastyMoon.Data.EF/EfRepository.cs
@@ -0,0 +1,53 @@
+﻿using ppedv.TastyMoon.DomainModel;
+using ppedv.TastyMoon.DomainModel.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.TastyMoon.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+
+        EfContext context = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Rezept))
+            //    context.Rezepte.Add(entity as Rezept);
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            //disconnected szenarien wie WCF, REST, WebAPI, ASP.MVC
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+            {
+                context.Entry(loaded).CurrentValues.SetValues(entity);
+            }
+        }
+    }
+}
---
+++ ppedv.TastyMoon/ppedv.TastyMoon.Logic/Core.cs
@@ -0,0 +1,25 @@
+﻿using ppedv.TastyMoon.DomainModel;
+using ppedv.TastyMoon.DomainModel.Contracts;
+using System;
+using System.Linq;
+
+namespace ppedv.TastyMoon.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repo)
+        {
+            Repository = repo;
+        }
+
+        public Rezept GetRezeptWithMostUsedMilk()
+        {
+            return Repository.GetAll<Rezept>().OrderBy(x => x.MilchMenge).FirstOrDefault();
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+    }
+}
